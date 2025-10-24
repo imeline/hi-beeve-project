78a6092c6d6d64d4e596f0bfacf09b18c3195c47
imeline <iseonju745@gmail.com>
2025-10-24 11:15:04 +0900

    init: swagger 파일 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.11")
 }
 
 kotlin {
--- src/main/kotlin/beeve/demo/DemoController.kt
+++ src/main/kotlin/beeve/demo/DemoController.kt
@@ -3,21 +3,27 @@ package beeve.demo
 import beeve.common.exception.ErrorStatus
 import beeve.common.exception.GlobalException
 import beeve.common.response.ApiResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Test", description = "테스트 API")
 @RestController
 @RequestMapping("/api/v1/test")
 class DemoController {
 
     // ✅ 정상 응답
+    // swagger 전역 토큰 설정이 필요없는 메소드 - @Operation(security = []) 추가
+    @Operation(summary = "성공 test", description = "정상 응답을 반환합니다."/*, security = []*/)
     @GetMapping("/success")
     fun success(): ApiResponse<String> {
         return ApiResponse.onSuccess("내보낼 데이터(DTO)가 들어가면 됨")
     }
 
     // ❌ 커스텀 비즈니스 예외 (GlobalException)
+    @Operation(summary = "커스텀 예외 test", description = "커스텀 예외를 발생시킵니다.")
     @GetMapping("/biz-error")
     fun bizError(): ApiResponse<Unit> {
         /** 아래 코드를 서비스 로직에서 **/
@@ -25,6 +31,7 @@ class DemoController {
     }
 
     // 💥 일반 런타임 예외 (핸들러에서 500 처리)
+    @Operation(summary = "핸들러 예외 test", description = "핸들러 감지 예외를 발생시킵니다.")
     @GetMapping("/runtime-error")
     fun runtimeError(): ApiResponse<Unit> {
         throw IllegalStateException("예상치 못한 런타임 예외!")
