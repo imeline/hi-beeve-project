7706d48e72689b7beee17efb7f6889d690102f44
sunju <iseonju745@gmail.com>
2025-10-29 16:23:08 +0900

    feat: PhysicalDataByAgeGroup Entity 생성

---
+++ src/main/kotlin/beeve/common/config/MongoConfig.kt
@@ -0,0 +1,8 @@
+package beeve.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.EnableMongoAuditing
+
+@Configuration
+@EnableMongoAuditing
+class MongoConfig
